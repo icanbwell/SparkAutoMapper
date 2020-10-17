be41ce464ccb29a26031117422c2a38494d1622f
imranq2 <imranq2@hotmail.com>
2020-10-16 19:28:21 -0700

    implement boolean

--- spark_auto_mapper/data_types/boolean.py
+++ spark_auto_mapper/data_types/boolean.py
@@ -2,7 +2,6 @@ from pyspark.sql import Column, DataFrame
 
 from spark_auto_mapper.data_types.column import AutoMapperDataTypeColumn
 from spark_auto_mapper.data_types.data_type_base import AutoMapperDataTypeBase
-from spark_auto_mapper.data_types.literal import AutoMapperDataTypeLiteral
 from spark_auto_mapper.helpers.value_parser import AutoMapperValueParser
 from spark_auto_mapper.type_definitions.defined_types import AutoMapperBooleanInputType
 
@@ -18,9 +17,9 @@ class AutoMapperBooleanDataType(AutoMapperDataTypeBase):
 
     def get_column_spec(self, source_df: DataFrame) -> Column:
         if isinstance(self.value, AutoMapperDataTypeColumn) \
-                or isinstance(self.value, AutoMapperDataTypeLiteral):
+                and dict(source_df.dtypes)[self.value.value] == "string":
             # parse the amount here
-            column_spec = self.value.get_column_spec(source_df=source_df)
+            column_spec = self.value.get_column_spec(source_df=source_df).cast("boolean")
             return column_spec
         else:
             column_spec = self.value.get_column_spec(source_df=source_df)
---
+++ tests/boolean/test_automapper_boolean.py
@@ -0,0 +1,54 @@
+from typing import Dict
+
+from pyspark.sql import SparkSession, Column, DataFrame
+# noinspection PyUnresolvedReferences
+from pyspark.sql.functions import col
+
+from spark_auto_mapper.automappers.automapper import AutoMapper
+from spark_auto_mapper.helpers.automapper_helpers import AutoMapperHelpers as A
+
+
+def test_auto_mapper_boolean(spark_session: SparkSession) -> None:
+    # Arrange
+    spark_session.createDataFrame(
+        [
+            (1, 'Qureshi', 'Imran', "0"),
+            (2, 'Vidal', 'Michael', "1"),
+        ],
+        ['member_id', 'last_name', 'first_name', "my_age"]
+    ).createOrReplaceTempView("patients")
+
+    source_df: DataFrame = spark_session.table("patients")
+
+    df = source_df.select("member_id")
+    df.createOrReplaceTempView("members")
+
+    # Act
+    mapper = AutoMapper(
+        view="members",
+        source_view="patients",
+        keys=["member_id"]
+    ).columns(
+        age=A.boolean(
+            A.column("my_age")
+        )
+    )
+
+    assert isinstance(mapper, AutoMapper)
+    sql_expressions: Dict[str, Column] = mapper.get_column_specs(source_df=source_df)
+    for column_name, sql_expression in sql_expressions.items():
+        print(f"{column_name}: {sql_expression}")
+
+    assert str(sql_expressions["age"]) == str(
+        col("my_age").cast("boolean").alias("age"))
+
+    result_df: DataFrame = mapper.transform(df=df)
+
+    # Assert
+    result_df.printSchema()
+    result_df.show()
+
+    assert result_df.where("member_id == 1").select("age").collect()[0][0] is False
+    assert result_df.where("member_id == 2").select("age").collect()[0][0] is True
+
+    assert dict(result_df.dtypes)["age"] == "boolean"
