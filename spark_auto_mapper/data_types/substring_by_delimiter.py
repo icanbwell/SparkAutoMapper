09bb055ee2c50ae80d7ba14c6ec48daae8ad1d9b
imranq2 <imranq2@hotmail.com>
2020-11-05 21:13:51 -0800

    add substring and substring_by_delimiter.py

---
+++ spark_auto_mapper/data_types/substring_by_delimiter.py
@@ -0,0 +1,30 @@
+from pyspark.sql import Column, DataFrame
+from pyspark.sql.functions import substring_index
+
+from spark_auto_mapper.data_types.column import AutoMapperDataTypeColumn
+from spark_auto_mapper.data_types.text_like_base import AutoMapperTextLikeBase
+
+
+class AutoMapperSubstringByDelimiterDataType(AutoMapperTextLikeBase):
+    """
+    Returns the substring from string str before count occurrences of the delimiter.
+    If count is positive, everything the left of the final delimiter (counting from left) is returned.
+    If count is negative, every to the right of the final delimiter (counting from the right) is returned.
+    substring_index performs a case-sensitive match when searching for delimiter.
+    """
+    def __init__(
+        self, column: AutoMapperDataTypeColumn, delimiter: str,
+        delimiter_count: int
+    ):
+        super().__init__()
+
+        self.column: AutoMapperDataTypeColumn = column
+        self.delimiter: str = delimiter
+        self.delimiter_count: int = delimiter_count
+
+    def get_column_spec(self, source_df: DataFrame) -> Column:
+        column_spec = substring_index(
+            self.column.get_column_spec(source_df=source_df), self.delimiter,
+            self.delimiter_count
+        )
+        return column_spec
---
+++ tests/right/test_automapper_right.py
@@ -0,0 +1,51 @@
+from typing import Dict
+
+from pyspark.sql import SparkSession, Column, DataFrame
+from pyspark.sql.functions import substring
+# noinspection PyUnresolvedReferences
+from pyspark.sql.functions import col
+
+from spark_auto_mapper.automappers.automapper import AutoMapper
+from spark_auto_mapper.helpers.automapper_helpers import AutoMapperHelpers as A
+
+
+def test_auto_mapper_right(spark_session: SparkSession) -> None:
+    # Arrange
+    spark_session.createDataFrame(
+        [
+            (1, 'Qureshi', 'Imran', "1970-01-01"),
+            (2, 'Vidal', 'Michael', "1970-02-02"),
+        ], ['member_id', 'last_name', 'first_name', "date_of_birth"]
+    ).createOrReplaceTempView("patients")
+
+    source_df: DataFrame = spark_session.table("patients")
+
+    df = source_df.select("member_id")
+    df.createOrReplaceTempView("members")
+
+    # Act
+    mapper = AutoMapper(
+        view="members", source_view="patients", keys=["member_id"]
+    ).columns(my_column=A.right(A.column("last_name"), 3))
+
+    assert isinstance(mapper, AutoMapper)
+    sql_expressions: Dict[str, Column] = mapper.get_column_specs(
+        source_df=source_df
+    )
+    for column_name, sql_expression in sql_expressions.items():
+        print(f"{column_name}: {sql_expression}")
+
+    assert str(sql_expressions["my_column"]) == str(
+        substring(col("b.last_name"), -3, 3).alias("my_column")
+    )
+
+    result_df: DataFrame = mapper.transform(df=df)
+
+    # Assert
+    result_df.printSchema()
+    result_df.show()
+
+    assert result_df.where("member_id == 1").select("my_column"
+                                                    ).collect()[0][0] == "shi"
+    assert result_df.where("member_id == 2").select("my_column"
+                                                    ).collect()[0][0] == "dal"
