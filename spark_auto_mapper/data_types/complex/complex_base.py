ac3ee0b6116ff71d292e7b0d263a10e706b8ba58
imranq2 <imranq2@hotmail.com>
2020-11-20 10:52:46 -0800

    change format for date string

--- spark_auto_mapper/data_types/complex/complex_base.py
+++ spark_auto_mapper/data_types/complex/complex_base.py
@@ -2,6 +2,8 @@ from typing import Dict, Any
 
 from pyspark.sql import Column, DataFrame
 from pyspark.sql.functions import struct
+
+from spark_auto_mapper.data_types.literal import AutoMapperDataTypeLiteral
 from spark_auto_mapper.helpers.value_parser import AutoMapperValueParser
 
 from spark_auto_mapper.data_types.data_type_base import AutoMapperDataTypeBase
@@ -34,7 +36,11 @@ class AutoMapperDataTypeComplexBase(AutoMapperDataTypeBase):
         return struct(
             [
                 self.get_value(value=value, source_df=source_df).alias(key)
-                for key, value in self.value.items()
-                if self.include_nulls or value is not None
+                for key, value in self.value.items() if self.include_nulls or (
+                    value is not None and not (
+                        isinstance(value, AutoMapperDataTypeLiteral)
+                        and value.value is None
+                    )
+                )
             ]
         )
