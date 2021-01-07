232b1979a39face12b8f88c2f0690b72872e4512
imranq2 <imranq2@hotmail.com>
2021-01-06 20:01:57 -0800

    fix field

--- spark_auto_mapper/data_types/field.py
+++ spark_auto_mapper/data_types/field.py
@@ -1,8 +1,8 @@
-from typing import Optional
+import re
+from typing import Optional, List
 
 from pyspark.sql import Column, DataFrame
 # noinspection PyUnresolvedReferences
-from pyspark.sql.functions import col, expr
 
 from spark_auto_mapper.data_types.array_base import AutoMapperArrayLikeBase
 
@@ -19,15 +19,18 @@ class AutoMapperDataTypeField(AutoMapperArrayLikeBase):
         self, source_df: DataFrame, current_column: Optional[Column]
     ) -> Column:
         if isinstance(self.value, str):
-            if self.value.startswith("_") and current_column is not None:
-                elements = self.value.split(".")
-                if len(elements) > 1:
-                    return current_column[elements[1]]
-                else:
-                    return current_column
-            elif "[" in self.value and "]" in self.value:
-                return expr(self.value)
+            if current_column is not None:
+                # noinspection RegExpSingleCharAlternation
+                elements: List[str] = re.split(r'\.|\[|]', self.value)
+                my_column: Column = current_column
+                for element in elements:
+                    if element != "_" and element != "":
+                        my_column = my_column[element if not element.isnumeric(
+                        ) else int(element)]
+                return my_column
             else:
-                return col(self.value)
+                raise ValueError(
+                    ".field() should only be used when iterating over an array"
+                )
 
         raise ValueError(f"value: {self.value} is not str")
---
+++ tests/field/test_automapper_field.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from typing import Dict
+
+from pyspark.sql import SparkSession, DataFrame, Column
+from tests.conftest import clean_spark_session
+
+from spark_auto_mapper.automappers.automapper import AutoMapper
+from spark_auto_mapper.helpers.automapper_helpers import AutoMapperHelpers as A
+# noinspection PyUnresolvedReferences
+
+
+def test_automapper_field(spark_session: SparkSession) -> None:
+    clean_spark_session(spark_session)
+    data_dir: Path = Path(__file__).parent.joinpath("./")
+
+    data_json_file: Path = data_dir.joinpath("data.json")
+
+    source_df: DataFrame = spark_session.read.json(
+        str(data_json_file), multiLine=True
+    )
+
+    source_df.createOrReplaceTempView("patients")
+
+    source_df.show(truncate=False)
+
+    # Act
+    mapper = AutoMapper(view="members", source_view="patients").columns(
+        age=A.column("identifier").select_one(A.field("type.coding[0].code"))
+    )
+
+    assert isinstance(mapper, AutoMapper)
+    sql_expressions: Dict[str, Column] = mapper.get_column_specs(
+        source_df=source_df
+    )
+    for column_name, sql_expression in sql_expressions.items():
+        print(f"{column_name}: {sql_expression}")
+
+    # assert str(sql_expressions["age"]
+    #            ) == str(col("b.identifier[0]").alias("age"))
+    result_df: DataFrame = mapper.transform(df=source_df)
+
+    result_df.show(truncate=False)
+
+    assert result_df.select("age").collect()[0][0] == "PRN"
