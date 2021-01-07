27b8947d326bc7c729fe724c07158ce8b1ec7f95
imranq2 <imranq2@hotmail.com>
2021-01-06 17:00:26 -0800

    add AutoMapperString

--- spark_auto_mapper/type_definitions/wrapper_types.py
+++ spark_auto_mapper/type_definitions/wrapper_types.py
@@ -1,11 +1,10 @@
 from typing import Union
 
-from spark_auto_mapper.data_types.text_like_base import AutoMapperTextLikeBase
-
-from spark_auto_mapper.data_types.data_type_base import AutoMapperDataTypeBase
 from spark_auto_mapper.data_types.column import AutoMapperDataTypeColumn
+from spark_auto_mapper.data_types.data_type_base import AutoMapperDataTypeBase
 from spark_auto_mapper.data_types.expression import AutoMapperDataTypeExpression
 from spark_auto_mapper.data_types.literal import AutoMapperDataTypeLiteral
+from spark_auto_mapper.data_types.text_like_base import AutoMapperTextLikeBase
 from spark_auto_mapper.type_definitions.native_types import AutoMapperNativeSimpleType, AutoMapperNativeListType
 
 AutoMapperWrapperType = Union[AutoMapperDataTypeLiteral,
