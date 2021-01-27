33529c3388ee20cf378283e761865c8dd464dcd3
imranq2 <imranq2@hotmail.com>
2021-01-27 15:21:06 -0800

    add to_float

--- spark_auto_mapper/data_types/datetime.py
+++ spark_auto_mapper/data_types/datetime.py
@@ -1,7 +1,7 @@
 from typing import Optional, List
 
 from pyspark.sql import Column, DataFrame
-from pyspark.sql.functions import coalesce, to_date
+from pyspark.sql.functions import coalesce, to_timestamp
 
 from spark_auto_mapper.data_types.column import AutoMapperDataTypeColumn
 from spark_auto_mapper.data_types.data_type_base import AutoMapperDataTypeBase
@@ -10,25 +10,20 @@ from spark_auto_mapper.helpers.value_parser import AutoMapperValueParser
 from spark_auto_mapper.type_definitions.defined_types import AutoMapperDateInputType
 
 
-class AutoMapperDateDataType(AutoMapperDataTypeBase):
+class AutoMapperDateTimeDataType(AutoMapperDataTypeBase):
     def __init__(
         self,
         value: AutoMapperDateInputType,
         formats: Optional[List[str]] = None
     ) -> None:
         """
-        Converts a value to date only
-        For datetime use the datetime mapper type
+        Converts the value to a timestamp type in Spark
 
 
         :param value: value
-        :param formats: (Optional) formats to use for trying to parse the value otherwise uses:
-                        y-M-d
-                        yyyyMMdd
-                        M/d/y
+        :param formats: (Optional) formats to use for trying to parse the value otherwise uses Spark defaults
         """
         super().__init__()
-        # keep string separate so we can parse it to date
 
         self.value: AutoMapperDataTypeBase = value \
             if isinstance(value, AutoMapperDataTypeBase) \
@@ -38,36 +33,23 @@ class AutoMapperDateDataType(AutoMapperDataTypeBase):
     def get_column_spec(
         self, source_df: Optional[DataFrame], current_column: Optional[Column]
     ) -> Column:
+        # if column is not of type date then convert it to date
         formats_column_specs: List[Column] = [
-            to_date(
+            to_timestamp(
                 self.value.get_column_spec(
                     source_df=source_df, current_column=current_column
                 ),
                 format=format_
             ) for format_ in self.formats
         ] if self.formats else [
-            to_date(
+            to_timestamp(
                 self.value.get_column_spec(
                     source_df=source_df, current_column=current_column
-                ),
-                format='y-M-d'
-            ),
-            to_date(
-                self.value.get_column_spec(
-                    source_df=source_df, current_column=current_column
-                ),
-                format='yyyyMMdd'
-            ),
-            to_date(
-                self.value.get_column_spec(
-                    source_df=source_df, current_column=current_column
-                ),
-                format='M/d/y'
+                )
             )
         ]
-        # if column is not of type date then convert it to date
         if source_df is not None and isinstance(self.value, AutoMapperDataTypeColumn) \
-                and not dict(source_df.dtypes)[self.value.value] == "date":
+                and not dict(source_df.dtypes)[self.value.value] == "timestamp":
             return coalesce(*formats_column_specs)
         elif isinstance(self.value, AutoMapperDataTypeLiteral):
             return coalesce(*formats_column_specs)
