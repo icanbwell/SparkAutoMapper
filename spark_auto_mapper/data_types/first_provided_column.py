c13b87fa4f905e76751439c2805655dd576e24bd
Matthew Printz <matt@icanbwell.com>
2021-04-02 16:46:16 -0500

    Add "First_provided_column" type
    Like coalesce, but works on columns/column definitions that exist/are valid, rather than the contents of said columns.
    Useful for data sources in which columns may be renamed at some point and you want to process files from before and after the name change or when columns are added at a point and are missing from earlier files.

---
+++ spark_auto_mapper/data_types/first_provided_column.py
@@ -0,0 +1,63 @@
+from typing import Generic, Optional, TypeVar, List
+
+from pyspark.sql import Column, DataFrame
+from pyspark.sql.utils import AnalysisException
+
+from spark_auto_mapper.data_types.data_type_base import AutoMapperDataTypeBase
+from spark_auto_mapper.helpers.value_parser import AutoMapperValueParser
+from spark_auto_mapper.type_definitions.wrapper_types import AutoMapperColumnOrColumnLikeType, AutoMapperAnyDataType
+
+_TAutoMapperDataType = TypeVar(
+    "_TAutoMapperDataType", bound=AutoMapperAnyDataType
+)
+
+
+class AutoMapperFirstProvidedColumnType(
+    AutoMapperDataTypeBase, Generic[_TAutoMapperDataType]
+):
+    """
+    Accepts any number of column definitions and will return the first valid column definition, similar to how
+    coalesce works, but with the existence of columns rather than null values inside the columns.
+
+    Useful for data sources in which columns may be renamed at some point and you want to process files from before and
+    after the name change or when columns are added at a point and are missing from earlier files.
+    """
+    def __init__(
+        self,
+        *columns: AutoMapperColumnOrColumnLikeType,
+    ):
+        super().__init__()
+
+        self.columns: List[AutoMapperColumnOrColumnLikeType] = [
+            AutoMapperValueParser.parse_value(column) for column in columns
+        ]
+
+    def get_column_spec(
+        self, source_df: Optional[DataFrame], current_column: Optional[Column]
+    ) -> Column:
+        column_spec = None
+
+        for column in self.columns:
+            try:
+                column_spec = column.get_column_spec(
+                    source_df=source_df, current_column=current_column
+                )
+                break
+            except Exception:
+                # By definition, if we are unable to resolve a column spec, for whatever reason, the column definition
+                # is not valid and should try the next column.
+                continue
+
+            col_name = column_spec._jc.toString(
+            )  # Get spark representation of the column
+            try:
+                # Force spark analyzer to confirm that column/expression is possible. This does not actually compute
+                # anything, just triggers the analyzer to check validity, which is what we want.
+                # If SparkSQL AnalysisException is thrown, continue to next column definition
+                if source_df:
+                    source_df.selectExpr(col_name.replace("b.", ""))
+                    break  # Break as soon as the above query doesn't error as we want the FIRST provided column
+            except AnalysisException:
+                continue
+
+        return column_spec
