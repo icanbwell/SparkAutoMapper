4bfaca203f18b1bfb2d0fff0bf7ad736159d3581
kyle <djjoebanks@gmail.com>
2023-07-27 14:58:45 -0700

    Dfp 1660 python 3 10 upgrade (#155)
    * update to  python 3-10
    
    * remove test that was mistakenly committed
    
    * remove test that was mistakenly committed
    
    ---------
    
    Co-authored-by: kyle paul <kyle@icanbwell.com>

--- tests/boolean/test_automapper_boolean.py
+++ tests/boolean/test_automapper_boolean.py
@@ -51,10 +51,16 @@ def test_auto_mapper_boolean(spark_session: SparkSession) -> None:
     result_df.printSchema()
     result_df.show()
 
-    assert result_df.where("member_id == 1").select("age", "is_active",).collect()[0][
+    assert result_df.where("member_id == 1").select(
+        "age",
+        "is_active",
+    ).collect()[0][
         :
     ] == (False, False)
-    assert result_df.where("member_id == 2").select("age", "is_active",).collect()[0][
+    assert result_df.where("member_id == 2").select(
+        "age",
+        "is_active",
+    ).collect()[0][
         :
     ] == (True, False)
 
